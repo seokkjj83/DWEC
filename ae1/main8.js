00a65dc3847afad0f659cf94cb6cc16997389048
hyngwar <seokkjj83@gmail.com>
2020-09-24 09:45:43 +0200

    AE1

---
+++ ae1/main8.js
@@ -0,0 +1,9 @@
+const rand=(Math.round(Math.random()*12));
+console.log(rand); 
+const num1=prompt("Dame un numero");
+
+do{
+    const num1=prompt("Dame un numero");
+}while(rand!=num1);
+
+alert("Los numeros "+num1+" han coincidido");
