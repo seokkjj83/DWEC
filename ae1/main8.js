34cb2dc116a0425b63b7cad32898755b27bf8a99
hyngwar <seokkjj83@gmail.com>
2020-09-24 10:04:26 +0200

    no message

--- ae1/main8.js
+++ ae1/main8.js
@@ -1,9 +1,11 @@
 const rand=(Math.round(Math.random()*12));
-console.log(rand); 
 const num1=prompt("Dame un numero");
-
 do{
-    const num1=prompt("Dame un numero");
+    console.log(rand);
+    if(num1<1 || num1>12){
+        const num1=prompt("Dame un numero entre el 1 y el 12");  
+
+    }
 }while(rand!=num1);
 
 alert("Los numeros "+num1+" han coincidido");
