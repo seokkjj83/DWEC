75651d097955cf0cd8bdcc71c570631363604c53
hyngwar <seokkjj83@gmail.com>
2020-11-06 16:37:37 +0100

    no message

--- AE3 - copia/explicacion jqery/math.js
+++
@@ -1,9 +0,0 @@
-const sum = (summand1, summand2) => {
-    return summand1 + summand2;
-}
-
-const substract = (number1, number2) => {
-    return number1 - number2;
-}
-
-module.exports = {sum, substract};
