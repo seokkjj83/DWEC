f4fa151fe663722c69fe0bcd575e9591e973657d
hyngwar <seokkjj83@gmail.com>
2020-12-04 15:41:35 +0100

    no message

---
+++ src/assets/js/main.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+
+
+const jobPromise = axios.get('https://jsonplaceholder.typicode.com/users');
+
+jobPromise.then((response) => {
+    console.log(response.data);
+
+});
--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-//  const axios = require('axios');
-
-
-const jobPromise = axios.get('https://jsonplaceholder.typicode.com/users');
-
-jobPromise.then((response) => {
-    console.log(response.data);
-
-});
