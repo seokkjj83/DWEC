ff08cbce551e0c9ab4821c1984e6dccf9cbb51dc
hyngwar <seokkjj83@gmail.com>
2020-12-18 12:48:29 +0100

    no message

---
+++ angular/src/app/products/product.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+  @Input() title = '';
+}
+//generacion de componentes via comando
+//ng generate component product
+//ng g c product
